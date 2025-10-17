bc9f4782990169382f1a211cddfd45faa1808a39
Jeff Pelletier <jpell185@yahoo.com>
2025-10-17 16:16:52 -0400

    add images, image component and flexbox

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,67 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 const peeps = [
   {
+    avatar: 'avatar-logan.png',
+    altText: 'Headshot of Wolverine with claws in front of his face',
     name: 'Wolverine',
     group: 'X-Men',
     id: 1,
   },
   {
+    avatar: 'avatar-tony.png',
+    altText: 'Headshot of Iron Man',
     name: 'Iron Man',
     group: 'Avengers',
     id: 2,
   },
   {
-    name: 'Colossus',
-    group: 'X-Men',
+    avatar: 'avatar-thor.png',
+    altText: 'Headshot of Thor with short hair',
+    name: 'Thor',
+    group: 'Avengers',
     id: 3,
   },
   {
+    avatar: 'avatar-cap.png',
+    altText: 'Headshot of Captain America with his mask off',
     name: 'Captain America',
     group: 'Avengers',
     id: 4,
   },
   {
+    avatar: 'avatar-spidey.png',
+    altText: 'Headshot of Spider-Man',
     name: 'Spider-Man',
     group: 'Avengers',
     id: 5,
   },
 ];
 
+export function Image({url, altText}) {
+  return (
+    <img className={styles.avatar} src={url} alt={altText} />
+  );
+}
+
 const listItems = peeps.map((peep) => (
   <li
-    className={styles.listItem}
     key={peep.id}
+    className={styles.listItem}
+    style={{
+      borderBottom: '1px solid #ccc',
+      padding: '10px'
+    }}
   >
     {peep.name}: {peep.group}
+    <Image url={peep.avatar} altText={peep.altText} />
   </li>
 ));
 
 export default function Home() {
   return (
-    <div >
-      <h1>Hellos</h1>
+    <div style={{padding: '10px'}}>
+      <h1 style={{marginBottom: '20px'}}>MCU Characters</h1>
       <ul className={styles.list}>
         {listItems}
       </ul>
